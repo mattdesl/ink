6b14495306d19f09d8295a75b9e791d58ad8aa84
mattdesl <dave.des@gmail.com>
2015-04-15 15:55:42 -0400

    use a local audio file for now to get around CORS

--- src/audio.js
+++ src/audio.js
@@ -3,9 +3,7 @@ import Analyser from 'gl-audio-analyser'
 
 module.exports = function(gl) {
   const audio = new Audio()
-  const ctx = new (window.AudioContext)()
-  let analyser 
-  // const analyser = Analyser(gl, audio)
+  const analyser = Analyser(gl, audio)
 
   badge({
     client_id: 'b95f61a90da961736c03f659c03cb0cc',
@@ -14,30 +12,17 @@ module.exports = function(gl) {
     getFonts: true
   }, function(err, src, json, div) {
     if (err) throw err
-      
-    console.log(src)
-    var source = document.createElement('source')
-    // source.src = src
-    audio.setAttribute('crossorigin', 'anonymous')
-    audio.src = src
-    // audio.appendChild(source)
-
-    // audio.loop = true
+    
+    //temp fix since CORS are not enabled on SoundCloud :(    
+    audio.src = 'audio/audio.mp3'
+    audio.loop = true
     audio.addEventListener('canplay', function() {
-      console.log("Yup", audio)
+      console.log("Playing audio...")
       audio.play()
     }, false)
-
-    audio.addEventListener('canplaythrough', function() {
-      setTimeout(function() {
-        analyser = Analyser(gl, audio, ctx)
-      }, 1000)
-      console.log("Yup2", audio)
-    })
   })
 
   return function waveform(shader) {
-    if (analyser)
-      shader.uniforms.iWaveform = analyser.bindWaveform(1)
+    shader.uniforms.iWaveform = analyser.bindWaveform(1)
   }
 }
